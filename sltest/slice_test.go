ffb3979929fec7fe41fc2b57bea8c579871d9c96
simpcl <simpcl2008@gmail.com>
2020-05-11 16:35:24 +0800

    add sltest

---
+++ sltest/slice_test.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestSlice1(t *testing.T) {
+	ss := []int{1, 2, 4}
+	ss = append(ss, 3)
+	t.Logf("%v", ss)
+}
+
+func TestSlice2(t *testing.T) {
+	ss := make([]int, 1, 2)
+	t.Logf("len: %d, cap: %d\n", len(ss), cap(ss))
+	ss[0] = 8
+	for count := 3; count > 0; count = count - 1 {
+		ss = append(ss, count)
+	}
+	t.Logf("%v", ss)
+}
